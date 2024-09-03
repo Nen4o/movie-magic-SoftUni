78ca93e0aa9e558835e73baa9f960e2c19391872
Minko Urchev <urchevminko@gmail.com>
2024-09-03 13:22:28 +0300

    add home page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const port = 5000;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Home page')
+})
+
 app.listen(port, () => {
     console.log('Server is listening on port ' + port);
 })
