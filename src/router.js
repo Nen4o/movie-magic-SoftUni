c826010c46eb283a2c99792ed6f77ce202c2d354
Minko Urchev <urchevminko@gmail.com>
2024-09-03 14:35:32 +0300

    add create page
    add movie controller

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeController = require('./controller/homeController');
+const movieController = require('./controller/movieController');
 
 router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
