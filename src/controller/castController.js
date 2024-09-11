9b625958c19bb6b42e4f279ac592d9c700f738ef
Minko Urchev <urchevminko@gmail.com>
2024-09-11 13:50:55 +0300

    add cast services

--- src/controller/castController.js
+++ src/controller/castController.js
@@ -4,4 +4,10 @@ router.get('/cast/create', (req, res) => {
     res.render('cast-create');
 })
 
+router.post('/cast/create', (req, res) => {
+    const cast = req.body;
+
+    console.log(cast);
+
+})
 module.exports = router;
