0311c744839a2b216d8047d89a49c37c782b3744
Minko Urchev <urchevminko@gmail.com>
2024-09-11 13:31:54 +0300

    add create cast

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const homeController = require('./controller/homeController');
 const movieController = require('./controller/movieController');
+const castController = require('./controller/castController');
 
 router.use(homeController);
 router.use(movieController);
+router.use(castController);
 
-router.get('*', (req, res)=>{
+router.get('*', (req, res) => {
     res.redirect('/404');
 })
 
