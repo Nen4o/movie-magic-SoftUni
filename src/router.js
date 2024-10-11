373c3dd5d6b00f3a6ff38efe95ca5cbceb64e540
Minko Urchev <urchevminko@gmail.com>
2024-10-11 09:09:50 +0300

    create register page

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controller/homeController');
 const movieController = require('./controller/movieController');
 const castController = require('./controller/castController');
+const authController = require('./controller/authController');
 
 router.use(homeController);
 router.use(movieController);
 router.use(castController);
+router.use(authController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
